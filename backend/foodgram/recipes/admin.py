5fce3462739302d1a00895dcccbb21a8c4c090cf
DIABLik666 <di-devil@yandex.ru>
2022-05-22 15:05:28 +0700

    Sprint: 14, Commit: 5

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -3,7 +3,18 @@ from django.contrib import admin
 from .models import Recipe
 
 
+class RecipeIngredientsInLine(admin.TabularInline):
+    model = Recipe.ingredients.through
+    extra = 1
+
+
+class RecipeTagsInLine(admin.TabularInline):
+    model = Recipe.tags.through
+    extra = 1
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'text', 'pub_date', 'author')
     search_fields = ('name', 'author')
+    inlines = (RecipeIngredientsInLine, RecipeTagsInLine)
